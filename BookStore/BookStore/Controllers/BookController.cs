5fe1d98c31c32582287fdc4457f30f93d480348d
DELL <rubanleo83@gmail.com>
2023-12-01 12:56:06 +0530

    Created Book Controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName,string authorName)
+        {
+            return $"Boom with name = {bookName} & Author = {authorName}";
+        }
+    }
+}
