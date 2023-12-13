a020e00a8477bc7286465aaa0b4362cd8eb8befe
DELL <rubanleo83@gmail.com>
2023-12-13 15:11:55 +0530

    Learned about dropdown statically

--- BookStore/BookStore/Models/BookModel.cs
+++ BookStore/BookStore/Models/BookModel.cs
@@ -4,20 +4,6 @@ namespace BookStore.Models
 {
     public class BookModel
     {
-        [DataType(DataType.DateTime)]
-        //[DataType(DataType.Date)]
-        //[DataType(DataType.Time)]
-        [Display(Name ="Choose Date and time")]
-        public string MyField { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name ="Enter your password")]
-        public string MyPassword { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        [Display(Name = "Enter your Email")]
-        [EmailAddress]
-        public string MyEmailAddress { get; set; }
         public int Id { get; set; }
         [StringLength(100, MinimumLength = 5)]
         [Required(ErrorMessage = "Please enter the Title of your book")]
@@ -27,6 +13,7 @@ namespace BookStore.Models
         [StringLength(500, MinimumLength = 30)]
         public string Description { get; set; }
         public string Category { get; set; }
+        [Required(ErrorMessage ="Please choose the language of your book")]
         public string Language { get; set; }
         [Required(ErrorMessage = "Please enter the Total pages")]
         [Display(Name ="Total pages of Book")]
--- BookStore/BookStore/Repository/BookRepository.cs
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -45,7 +45,8 @@ namespace BookStore.Repository
                 CreatedOn = DateTime.UtcNow,
                 Description = model.Description,
                 Title = model.Title,
-                TotalPages = model.TotalPages,
+                Language = model.Language,
+                TotalPages = model.TotalPages.HasValue ? model.TotalPages.Value : 0,
                 UpdatedOn = DateTime.UtcNow
             };
 
