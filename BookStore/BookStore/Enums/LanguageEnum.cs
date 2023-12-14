d2a1126d7d4ba8b6a943509fc40f375f4574de9d
DELL <rubanleo83@gmail.com>
2023-12-14 08:43:08 +0530

    Learned about passing data to dropdown via Enums

--- BookStore/BookStore/Enums/LanguageEnum.cs
+++ BookStore/BookStore/Enums/LanguageEnum.cs
@@ -4,15 +4,15 @@ namespace BookStore.Enums
 {
     public enum LanguageEnum
     {
-        [Display(Name="Hindi Language")]
-        Hindi=10,
+        [Display(Name = "Hindi Language")]
+        Hindi = 10,
         [Display(Name = "English Language")]
-        English =11,
+        English = 11,
         [Display(Name = "German Language")]
-        German =12,
+        German = 12,
         [Display(Name = "Chinese Language")]
-        Chinese =13,
+        Chinese = 13,
         [Display(Name = "Urdu Language")]
-        Urdu =14
+        Urdu = 14
     }
 }
