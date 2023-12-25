f8a28c2ebbe12870279c32fa859bf6054392d334
jerome <zachariah11jerobert@gmail.com>
2023-12-26 05:15:21 +0530

    learned about binding configuration

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace BookStore.Controllers
@@ -14,22 +15,11 @@ namespace BookStore.Controllers
 
         public ViewResult Index()
         {
+            var newBookAlert = new NewBookAlertConfig();
+            _configuration.Bind("NewBookAlert", newBookAlert);
 
-
-            //var result = _configuration["AppName"];
-            //var key1 = _configuration["infoObj:key1"];
-            //var key2 = _configuration["infoObj:key2"];
-            //var key3 = _configuration["infoObj:key3:key3obj1"];
-
-            var result1 = _configuration["DisplayNewBookAlert"];
-            var result2 = _configuration.GetValue<bool>("DisplayNewBookAlert");
-            var result3 = _configuration.GetValue<bool>("NewBookAlert:DisplayNewBookAlert");
-            var bookName = _configuration.GetValue<string>("NewBookAlert:BookName");
-
-            // GetSection
-            var newBookAlert = _configuration.GetSection("NewBookAlert");
-            var displayNewBookAlert = newBookAlert.GetValue<bool>("DisplayNewBookAlert");
-            var newBookName = newBookAlert.GetValue<string>("BookName");
+            bool isDisplay = newBookAlert.DisplayNewBookAlert;
+         
             return View();
         }
 
